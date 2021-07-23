c33d216a66b7ef48eecf256a66b7b5bcdf89b117
Rius2g <marius.mollerhansen@hotmail.com>
2021-07-23 14:11:05 +0200

    fixed 368B

--- B/368B.cpp
+++ B/368B.cpp
@@ -5,27 +5,27 @@ int main()
 {
     int n, m, t;
     cin >> n >> m;
-
     int a[100005], d[100005] = {0};
     bool v[100005];
 
-    for(int i=0;i<=n;i++)   
+    for (int i = 1; i <= n; i++)
         cin >> a[i];
 
-    for(int i =n; i>=1;i--)
+    for (int i = n; i >= 1; i--)
     {
         if (!v[a[i]])
-            d[i]++;
+          d[i]++;
         v[a[i]] = true;
     }
-    for(int i=n; i>= 1; i--)
-        d[i] += d[i +1];
 
-    for(int i = 0; i < m; i++)
+    for (int i = n; i >= 1; i--)
+        d[i] += d[i + 1];
+
+    for (int i = 0; i < m; i++)
     {
         cin >> t;
         cout << d[t] << endl;
     }
-    return 0;
 
+    return 0;
 }
